7b150c691ac459796d18ef7125912f442cbd9407
kandalog <asuasu.tee@gmail.com>
2022-03-25 22:58:14 +0900

    プロフィール編集ページを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,17 @@ class User < ApplicationRecord
 
   # アップローダーを紐付け
   mount_uploader :image, ImageUploader
+
+  def update_without_current_password(params, *options)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+  
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
   root 'static_pages#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   get '/users/:id', to: 'users#show', as: 'user'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
